e0574f298645b38e3db779d622146182cd271ece
SHABIN7009 <shabintc@bpract.com>
2020-10-20 09:57:04 +0530

    notification sending

--- resources/views/emails/payout.blade.php
+++ resources/views/emails/payout.blade.php
@@ -4,7 +4,7 @@
 
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
 
-        <title> Company Name - </title>
+        <title></title>
 
     </head>
 
@@ -16,7 +16,7 @@
 
             <table border="0" cellpadding="0" align="center" cellspacing="0" class="responsive-table">
 
-            <tbody><tr><td >{{$content}}</td></tr>
+            <tbody><tr><td></td></tr>
 
             </tbody></table>
 
@@ -26,6 +26,52 @@
 
 </tbody></table>
 
-      
+        <table width="600" bgcolor="#f9f9f9" style="font-family: MyriadPro-Regular, 'Myriad Pro Regular', MyriadPro, 'Myriad Pro', Helvetica, Arial, sans-serif;">
+
+            <thead>
+
+                <tr>
+
+                  <td style="padding:22px 30px 18px 30px; border-bottom:2px solid #1e2064"><img src="{{ url('files/images/icons/logo.png') }}" height="60" alt="Dream makers home"></td>   
+
+
+                </tr>
+
+            </thead>
+
+            <tbody>
+
+               
+
+                <tr>
+
+                    <td style="padding:0px 30px 20px 30px;"><p style="font-size:14px; color:#666666;">
+
+
+                  Hi {{$username}} 
+
+
+ <p>{{$content}}</p>
+
+
+
+                    </td>
+
+                </tr>
+
+                <tr>
+
+                
+                </tr>
+            </tbody>
+
+            <tfoot bgcolor="#c1c1c1">
+
+               
+
+            </tfoot>
+
+        </table>
+
 </body>
   </html>
